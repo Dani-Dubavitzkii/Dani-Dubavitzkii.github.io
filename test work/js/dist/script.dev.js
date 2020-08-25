87ba97648887d7d9a96e4b441dc27910b277945f
Dani-Dubavitzkii <65012263+Dani-Dubavitzkii@users.noreply.github.com>
2020-08-25 17:03:48 +0300

    Add files via upload

---
+++ test work/js/dist/script.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.btn').click(function () {
+    $(this).next().children('p').slideToggle(500);
+    /* if($(this).next().children('p').is(':visible')){
+    	$(this).attr('class', 'btn-active');
+    } else{
+    	$(this).removeAttr('class').attr('class', 'btn');
+    } */
+  });
+});
